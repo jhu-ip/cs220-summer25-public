d89f654bb6d5f14f41d9b1de01748e97d182ede6
Paul D. Martin, Ph.D. <1087972+pauldmartinphd@users.noreply.github.com>
2025-06-24 21:00:03 -0400

    Add files via upload

---
+++ homeworks/hw5/phone_db.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <map>
+#include <cctype>
+
+using std::vector;
+using std::string;
+using std::map;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+  // TODO: implement
+
+  return 0;
+}
---
+++ homeworks/hw7/CTrie.cpp
@@ -0,0 +1,11 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+// TODO: add other #includes as needed
+
+#include "CTrie.h"
+
+// TODO: implement the CTrie member functions and friend functions
